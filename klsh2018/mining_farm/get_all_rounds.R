5bebb9ff11b2ecb21ceee57de7a3cd1bfd187e96
Boris Demeshev <boris.demeshev@gmail.com>
2018-07-24 01:15:58 +0700

    update day n+1

--- klsh2018/mining_farm/get_all_rounds.R
+++ klsh2018/mining_farm/get_all_rounds.R
@@ -1,6 +1,6 @@
 library(rmarkdown)
 
-for (seed in 1:10) {
+for (seed in 1:20) {
   rmarkdown::render("minin_farm.Rmd", params = list(the_seed = seed))
   file.copy(from = "minin_farm.pdf", to = paste0("farm_", seed, ".pdf", collapse = ""))
 }
